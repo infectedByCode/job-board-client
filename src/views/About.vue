c186f0d67e0191fa8e29844dff8981596adcfe3a
Matt Daley <mdaleyhk@gmail.com>
2020-10-15 20:22:38 +0100

    initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,28 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <ul>
+      <li v-for="job in jobs" :key="job.jobId">
+        {{ job.jobId }}
+      </li>
+    </ul>
   </div>
 </template>
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      jobs: null,
+    };
+  },
+  created() {
+    axios.get('http://localhost:3000/jobs').then((response) => {
+      if (response.status === 200 && response.data) {
+        this.jobs = response.data.jobs;
+      }
+    });
+  },
+};
+</script>
