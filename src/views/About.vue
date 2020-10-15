9d48887b9eef32f89cbd3e5d7b7cad15df178475
Matt Daley <mdaleyhk@gmail.com>
2020-10-15 20:43:09 +0100

    get basic job board layout

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <h1>Jobs</h1>
     <ul>
-      <li v-for="job in jobs" :key="job.jobId">
-        {{ job.jobId }}
+      <li v-for="job in jobs" :key="job.jobId" :class="$style.jobCard">
+        <h2>{{ job.jobTitle }}</h2>
+        <p>{{ job.companyName }}</p>
+        <span>
+          <p>
+            {{ job.salary }}
+          </p>
+          <p>
+            {{ job.added }}
+          </p>
+        </span>
       </li>
     </ul>
   </div>
 </template>
 <script>
 import axios from 'axios';
+import { differenceInCalendarDays } from 'date-fns';
 
 export default {
+  name: 'Jobs',
   data() {
     return {
       jobs: null,
@@ -21,8 +32,46 @@ export default {
     axios.get('http://localhost:3000/jobs').then((response) => {
       if (response.status === 200 && response.data) {
         this.jobs = response.data.jobs;
+        this.jobs.map((job) => {
+          job.added = this.formatDate(job.createdAt);
+          return job;
+        });
       }
     });
   },
+  methods: {
+    formatDate(date) {
+      const numOfDays = differenceInCalendarDays(new Date(), new Date(date));
+      return `${numOfDays} day${numOfDays > 1 ? 's' : ''} ago`;
+    },
+  },
 };
 </script>
+<style module>
+ul {
+  padding: 0;
+}
+
+.jobCard {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin: 10px auto;
+  padding: 2.5% 7.5%;
+  height: 150px;
+  width: 60%;
+  background: hotpink;
+  list-style: none;
+}
+
+.jobCard > h2,
+.jobCard > p {
+  margin: 0;
+  line-height: 36px;
+}
+
+.jobCard > span {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
