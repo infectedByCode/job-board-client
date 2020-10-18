a6d9001fee574c0078309f0a9d685e6af1b9f243
Matt Daley <mdaleyhk@gmail.com>
2020-10-18 16:16:06 +0100

    add http request scaffold for auth

--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -3,14 +3,14 @@
     <!-- TODO: Add keywords -->
     <form>
       <!-- TODO: extract radio set -->
-      <fieldset v-if="currentPath === '/signup'" :class="$style.accountType">
+      <fieldset :class="$style.accountType">
         <label for="jobseeker">Jobseeker</label>
-        <input id="jobseeker" type="radio" value="jobseeker" v-model="accountType" />
+        <input id="jobseeker" type="radio" value="jobseekers" v-model="accountType" />
         <label for="jobseeker">Company</label>
-        <input id="company" type="radio" value="company" v-model="accountType" />
+        <input id="company" type="radio" value="companies" v-model="accountType" />
       </fieldset>
       <TextInput
-        v-if="currentPath === '/signup' && accountType === 'jobseeker'"
+        v-if="currentPath === '/signup' && accountType === 'jobseekers'"
         v-model="forename"
         name="forename"
         size="max"
@@ -18,7 +18,7 @@
         align="left"
       />
       <TextInput
-        v-if="currentPath === '/signup' && accountType === 'jobseeker'"
+        v-if="currentPath === '/signup' && accountType === 'jobseekers'"
         v-model="surname"
         name="surname"
         size="max"
@@ -26,7 +26,7 @@
         align="left"
       />
       <TextInput
-        v-if="currentPath === '/signup' && accountType === 'company'"
+        v-if="currentPath === '/signup' && accountType === 'companies'"
         v-model="companyName"
         name="companyName"
         size="max"
@@ -34,7 +34,7 @@
         align="left"
       />
       <TextInput
-        v-if="currentPath === '/signup' && accountType === 'company'"
+        v-if="currentPath === '/signup' && accountType === 'companies'"
         v-model="companyAddress"
         name="companyAddress"
         size="max"
@@ -42,7 +42,7 @@
         align="left"
       />
       <TextInput
-        v-if="currentPath === '/signup' && accountType === 'company'"
+        v-if="currentPath === '/signup' && accountType === 'companies'"
         v-model="companyPhone"
         name="companyPhone"
         size="max"
@@ -50,7 +50,14 @@
         align="left"
       />
       <TextInput v-model="email" name="email" size="max" label="Email" align="left" />
-      <TextInput v-model="password" name="password" size="max" label="Password" align="left" />
+      <TextInput
+        v-model="password"
+        name="password"
+        size="max"
+        label="Password"
+        align="left"
+        type="password"
+      />
       <TextInput
         v-if="currentPath === '/signup'"
         v-model="confirmpw"
@@ -58,7 +65,9 @@
         size="max"
         label="Confirm password"
         align="left"
+        type="password"
       />
+      <button @click.prevent="handleSubmit">Click</button>
     </form>
     <router-link v-if="currentPath === '/signup'" to="/login">Already registered? Click here.</router-link>
     <router-link v-if="currentPath === '/login'" to="/signup">Not registered? Click here.</router-link>
@@ -66,6 +75,7 @@
 </template>
 
 <script>
+import axios from "axios";
 import TextInput from "../components/TextInput";
 
 export default {
@@ -89,6 +99,32 @@ export default {
   computed: {
     currentPath() {
       return this.$route.path;
+    }
+  },
+  methods: {
+    handleSubmit() {
+      const userId = "6778f925-d847-498a-89d6-1702fb3abbf7";
+      const route =
+        this.currentPath === `/login` ? "auth/login" : this.accountType;
+      const data = {
+        password: this.password,
+        jobseekerForename: this.forename,
+        jobseekerSurname: this.surname,
+        companyName: this.companyName,
+        companyAddress: this.companyAddress,
+        companyPhone: this.companyPhone,
+        role: this.accountType === "jobseekers" ? "jobseeker" : "company"
+      };
+      data[
+        this.accountType === "jobseekers" ? "jobseekerEmail" : "companyEmail"
+      ] = this.email;
+      axios
+        .post(`http://localhost:3000/${route}`, { ...data })
+        .then(response => {
+          if (response.status === 200 && response.data) {
+          }
+        })
+        .catch(err => console.log(err));
     }
   }
 };
