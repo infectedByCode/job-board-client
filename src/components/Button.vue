fa20d8670911c253e83d93b3c9d23874292a3f19
Matt Daley <mdaleyhk@gmail.com>
2020-10-29 20:04:21 +0000

    extract button to component to recycle styles

---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button :class="$style.customButton" @click.prevent="handleClick">
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleClick() {
+      this.$emit("click");
+    }
+  }
+};
+</script>
+
+<style module>
+.customButton {
+  background-color: #dd5800;
+  display: block;
+  width: 100%;
+  height: 30px;
+  line-height: 30px;
+  margin: 15% auto;
+  border-radius: 6px;
+}
+</style>
