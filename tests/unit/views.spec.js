4943cfaee7f35ff5cee76f1a0a9b55f0b95fc2fd
Matt Daley <mdaleyhk@gmail.com>
2020-11-23 21:05:42 +0000

    complete testing on Home view

--- tests/unit/views.spec.js
+++ tests/unit/views.spec.js
@@ -1,7 +1,14 @@
-import { render, cleanup } from '@testing-library/vue';
+import { render, cleanup, fireEvent, waitFor } from '@testing-library/vue';
 import '@testing-library/jest-dom';
 
+import App from '../../src/App.vue';
 import Home from '../../src/views/Home.vue';
+import JobBoard from '../../src/views/JobBoard.vue';
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/jobs', name: 'Job Board', component: JobBoard },
+];
 
 describe('Views', () => {
   afterEach(() => {
@@ -15,6 +22,14 @@ describe('Views', () => {
       getByLabelText('Where');
       getByRole('button', { name: 'Search' });
       // Add select
+    });
+    it('submits the form with search term typed', async () => {
+      const { getByRole, getByLabelText } = render(Home, { routes });
+      expect(location.href).not.toContain('/jobs');
+      const input = getByLabelText('What');
+      await fireEvent.update(input, 'developer');
+      fireEvent.click(getByRole('button', { name: 'Search' }));
+      expect(location.href).toContain('/jobs');
     });
   });
 });
