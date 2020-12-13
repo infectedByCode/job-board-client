209577c6fdc9f3d0c303b95f85308a9df5c209cf
Matt Daley <mdaleyhk@gmail.com>
2020-12-13 13:51:35 +0000

    create basic outline of job advert with tests

--- src/views/JobAdvert.vue
+++ src/views/JobAdvert.vue
@@ -1,20 +1,50 @@
 <template>
-  <div>
-    <h1>Hello</h1>
-    <h3>World</h3>
-  </div>
+  <main :class="$style.advertContainer">
+    <section :class="$style.jobContainer">
+      <h1>{{ jobDetails.jobTitle }}</h1>
+      <div>
+        <p>Location: {{ jobDetails.jobLocation }}</p>
+        <p>Salary: £{{ jobDetails.salary }}</p>
+        <p>Posted {{ jobDetails.added }}</p>
+      </div>
+      <ul>
+        <li v-for="(tag, i) in jobDetails.tags.split(',')" :key="i" :class="$style.tag">{{ tag }}</li>
+      </ul>
+
+      <p>{{ jobDetails.jobText }}</p>
+    </section>
+    <section :class="$style.companyContainer">
+      <p>{{ jobDetails.companyName }}</p>
+      <Button>Email</Button>
+      <Button>Call</Button>
+    </section>
+  </main>
 </template>
 
 <script>
+import Button from '../components/Button';
 export default {
   props: {
     jobDetails: {
       type: Object,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
 };
 </script>
 
 <style module>
+.jobContainer,
+.companyContainer {
+  margin: 30px 0 0 0;
+}
+
+.tag {
+  display: inline-block;
+  list-style: none;
+  margin: 5px;
+  padding: 5px;
+  border-radius: 5px;
+  background-color: cadetblue;
+}
 </style>
--- tests/unit/views.spec.js
+++ tests/unit/views.spec.js
@@ -1,4 +1,4 @@
-import { render, cleanup, fireEvent, waitFor } from '@testing-library/vue';
+import { render, cleanup, fireEvent, waitFor, getAllByText, getAllByRole } from '@testing-library/vue';
 import '@testing-library/jest-dom';
 import axios from 'axios';
 jest.mock('axios');
@@ -6,12 +6,14 @@ jest.mock('axios');
 import App from '../../src/App.vue';
 import Home from '../../src/views/Home.vue';
 import JobBoard from '../../src/views/JobBoard.vue';
+import JobAdvert from '../../src/views/JobAdvert.vue';
 
 import { fakeJobs } from '../../src/mocks/faker';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/jobs', name: 'Job Board', component: JobBoard },
+  { path: '/jobs/:jobId', name: 'Job Advert', component: JobAdvert },
 ];
 
 describe('Views', () => {
@@ -64,6 +66,40 @@ describe('Views', () => {
       });
       const listItems = await findAllByRole('listitem');
       expect(listItems).toHaveLength(5);
+    });
+  });
+  describe('JobAdvert', () => {
+    it('renders job advertisement text to client', () => {
+      const jobDetails = {
+        added: '4 days ago',
+        applyEmail: 'myemail@email.com',
+        closingDate: '2021-09-01T23:00:00.000Z',
+        companyAddress: '123 Dev Street',
+        companyEmail: 'email@company.com',
+        companyName: 'Ample Devs',
+        companyPhone: '12345678',
+        jobLocation: 'Manchester',
+        jobText: 'A developer with over 200 years experience required for a startup.',
+        jobTitle: 'Web Developer',
+        salary: 15000,
+        tags: 'MySQL,NodeJS',
+      };
+      const { getByText, getByRole, getAllByRole } = render(JobAdvert, {
+        routes,
+        props: { jobDetails },
+      });
+      getByText(jobDetails.jobText);
+      getByText(jobDetails.jobTitle);
+      getByText(`Posted ${jobDetails.added}`);
+      getByText(`Salary: £${jobDetails.salary}`);
+      getByText(`Location: ${jobDetails.jobLocation}`);
+      const listItems = getAllByRole('listitem');
+      expect(listItems).toHaveLength(2);
+      expect(listItems[0]).toHaveTextContent('MySQL');
+      expect(listItems[1]).toHaveTextContent('NodeJS');
+      getByText(jobDetails.companyName);
+      getByRole('button', { name: 'Email' });
+      getByRole('button', { name: 'Call' });
     });
   });
 });
